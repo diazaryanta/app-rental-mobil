fe298d09aeeb95f31835d06df6f33752bd00ac42
Diaz Aryanta <71869993+diazaryanta@users.noreply.github.com>
2021-12-02 12:44:44 +0700

    Update Data_customer.php

--- application/controllers/admin/Data_customer.php
+++ application/controllers/admin/Data_customer.php
@@ -6,7 +6,7 @@ class Data_customer extends CI_Controller{
         $data['customer'] = $this->rental_model->get_data('customer')->result();
         $this->load->view('templates_admin/header');
         $this->load->view('templates_admin/sidebar');
-        $this->load->view('admin/data_customer');
+        $this->load->view('admin/data_customer',$data);
         $this->load->view('templates_admin/footer');
     }
 
