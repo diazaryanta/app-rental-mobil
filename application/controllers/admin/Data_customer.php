512fc3230cd20cf5cd223f1891073d043986ff5b
diazat <diazaryanta04@yahoo.com>
2021-12-02 14:25:11 +0700

    update new file

--- application/controllers/admin/Data_customer.php
+++ application/controllers/admin/Data_customer.php
@@ -6,7 +6,7 @@ class Data_customer extends CI_Controller{
         $data['customer'] = $this->rental_model->get_data('customer')->result();
         $this->load->view('templates_admin/header');
         $this->load->view('templates_admin/sidebar');
-        $this->load->view('admin/data_customer');
+        $this->load->view('admin/data_customer',$data);
         $this->load->view('templates_admin/footer');
     }
 
@@ -62,6 +62,59 @@ class Data_customer extends CI_Controller{
         $this->load->view('templates_admin/sidebar');
         $this->load->view('admin/form_update_customer',$data);
         $this->load->view('templates_admin/footer');
+    }
+
+    public function update_customer_aksi()
+    {
+        $this->_rules();
+
+        if($this->form_validation->run() == FALSE) {
+            $this->update_customer_aksi();
+        }else{
+            $id                         = $this->input->post('id_customer');
+            $nama                       = $this->input->post('nama');
+            $username                   = $this->input->post('username');
+            $alamat                     = $this->input->post('alamat');
+            $gender                     = $this->input->post('gender');
+            $no_telefon                 = $this->input->post('no_telefon');
+            $no_ktp                     = $this->input->post('no_ktp');
+            $password                   = md5($this->input->post('password'));
+
+            $data = array (
+                'nama'                  => $nama,
+                'username'              => $username,
+                'alamat'                => $alamat,
+                'gender'                => $gender,
+                'no_telefon'            => $no_telefon,
+                'no_ktp'                => $no_ktp,
+                'password'              => $password
+
+            );
+
+            $where = array(
+                'id_customer' => $id
+            );
+
+            $this->rental_model->update_data('customer',$data,$where);
+            $this->session->set_flashdata('pesan','<div class="alert alert-success alert-dismissible fade show" role="alert">
+                        Data Customer Berhasil Diupdate!
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span></buttom>
+                        </div>');
+            redirect('admin/data_customer');
+        }
+    }
+
+    public function delete_customer($id)
+    {
+        $where = array('id_customer' => $id);
+        $this->rental_model->delete_data($where, 'customer');
+        $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        Data Customer Berhasil Dihapus!
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span></buttom>
+                        </div>');
+        redirect('admin/data_customer');
     }
 
     public function _rules()
--- application/views/admin/form_update_customer.php
+++ application/views/admin/form_update_customer.php
@@ -4,53 +4,57 @@
             <h1>Data Customer</h1>
         </div>
     </section>
-    <form method="POST" action="<?php echo base_url('admin/data_customer/tambah_customer_aksi') ?>">
+
+    <?php foreach ($customer as $cs) : ?>
+    <form method="POST" action="<?php echo base_url('admin/data_customer/update_customer_aksi') ?>">
 
         <div class="form-group">
             <label>Nama</label>
-            <input type="text" name="nama" class="form-control">
+            <input type="hidden" name="id_customer" value="<?php echo $cs->id_customer?>">
+            <input type="text" name="nama" class="form-control" value="<?php echo $cs->nama?>">
             <?php echo form_error('nama','<span class="text-small text-danger">','</span>') ?>
         </div>
 
         <div class="form-group">
             <label>Username</label>
-            <input type="text" name="username" class="form-control">
+            <input type="text" name="username" class="form-control" value="<?php echo $cs->username?>">
             <?php echo form_error('username','<span class="text-small text-danger">','</span>') ?>
         </div>
 
         <div class="form-group">
             <label>Alamat</label>
-            <input type="text" name="alamat" class="form-control">
+            <input type="text" name="alamat" class="form-control" value="<?php echo $cs->alamat?>">
             <?php echo form_error('alamat','<span class="text-small text-danger">','</span>') ?>
         </div>
 
         <div class="form-group">
             <label>Gender</label>
             <select class="form-control" name="gender">
-                <option value="">-- Pilih Gender --</option>
+                <option value="<?php echo $cs->gender?>"><?php echo $cs->gender?></option>
                 <option value="Laki-Laki">Laki-laki</option>
                 <option value="perempuan">Perempuan</option>
             </select>
 
         <div class="form-group">
             <label>No. Telefon</label>
-            <input type="text" name="no_telefon" class="form-control">
+            <input type="text" name="no_telefon" class="form-control" value="<?php echo $cs->no_telefon?>">
             <?php echo form_error('no_telefon','<span class="text-small text-danger">','</span>') ?>
         </div>
 
         <div class="form-group">
             <label>No. KTP</label>
-            <input type="text" name="no_ktp" class="form-control">
+            <input type="text" name="no_ktp" class="form-control" value="<?php echo $cs->no_ktp?>">
             <?php echo form_error('no_ktp','<span class="text-small text-danger">','</span>') ?>
         </div>
 
         <div class="form-group">
             <label>Password</label>
-            <input type="password" name="password" class="form-control">
+            <input type="password" name="password" class="form-control" value="<?php echo $cs->password?>">
             <?php echo form_error('password','<span class="text-small text-danger">','</span>') ?>
         </div>
 
         <button type="submit" class="btn btn-sm btn-primary" >Submit</button>
         <button type="reset" class="btn btn-sm btn-danger" >Reset</button>
     </form>
+    <?php endforeach; ?>
 </div>
